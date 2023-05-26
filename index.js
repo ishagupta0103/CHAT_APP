2aa6a4abe5aa42f743916ac3ccdbb5ba7e48c230
Isha <ishagupta.1002@gmail.com>
2023-05-26 17:04:55 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    socket.on('chat message', function (mes) {
+        io.emit('chat message', mes);
+    });
+});
+
+
+http.listen(1908, function () {
+    console.log('Listening to port 1908');
+});
